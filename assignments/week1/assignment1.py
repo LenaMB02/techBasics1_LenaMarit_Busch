83fe63e25e450858249889aee287f5cfba240c70
LenaMB02 <lena.m.busch@stud.leuphana.de>
2026-04-18 10:48:32 +0200

    Update assignment1.py

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,1 +1,18 @@
+#%%
+'''
+This code prints a simple ASCII art tennis ball.
+I used the letter O for the ball shape.
+I used simple shapes to keep it readable.
+'''
 
+print("   OOOOO   ")
+
+print(" OO     OO ")
+
+print("O         O")
+
+print("O         O")
+
+print(" OO     OO ")
+
+print("   OOOOO   ")
