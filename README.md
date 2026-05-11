9168ae4ef1ae2130f55f15bca1d3a2c2e3977eb2
LenaMB02 <lena.m.busch@stud.leuphana.de>
2026-05-11 09:55:36 -0400

    Update week 1: before pulling new week

--- README.md
+++
@@ -1,4 +0,0 @@
-# Tech Basics 1
-GitHub Repository for **Tech Basics 1**, 2026 SoSe
-
-Inside a README file, one writes about some basic Info about the repository.
---
+++ assignments/week1/assignment1.py
@@ -0,0 +1,24 @@
+#%%
+'''
+This code prints a simple ASCII art tennis ball.
+I used the letter O for the ball shape.
+I used simple shapes to keep it readable.
+'''
+
+print("   OOOOO   ")
+
+print(" OO     OO ")
+
+print("O         O")
+
+print("O         O")
+
+print(" OO     OO ")
+
+print("   OOOOO   ")
+
+'''
+I wanted to make something related to my passion for playing tennis.
+I managed to mage it look like a ball, although it could be improved to resemble a real tennis ball more closely.
+Overall, creating ASCII art for the first time was an interesting and enjoayble experience.
+'''
